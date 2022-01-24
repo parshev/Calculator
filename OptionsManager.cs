b411482ac010bc6159a1063f568071db8a7bdab1
Krasimir23 <98305141+Krasimir23@users.noreply.github.com>
2022-01-24 10:45:08 +0200

    Add files via upload

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -13,6 +13,37 @@ namespace GitExercise
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
 
-       
+        public static string[] OptionsList = {
+            "s- Subtract"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a -b}");
+
+        }
+
+            public static string[] OptionsList = {
+            "m - Multyply"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+
+        public static string[] OptionsList = {
+            "k -Devide "
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+
+
+
+
+
     }
 }
